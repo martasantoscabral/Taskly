fe8bd584b9646f017c902d2d6d56957f39b23e88
MartaCabral <martasantoscabral@gmail.com>
2026-07-20 17:03:33 +0100

    Adicionar migrations da base de dados

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,16 @@
-import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const connectionString = process.env.DATABASE_URL;
 
-const adapter = new PrismaPg({ connectionString });
+if (!connectionString) {
+  throw new Error("DATABASE_URL não está definida.");
+}
 
-export const prisma = new PrismaClient({ adapter });
+const adapter = new PrismaPg({
+  connectionString,
+});
+
+export const prisma = new PrismaClient({
+  adapter,
+});
